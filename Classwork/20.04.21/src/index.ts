4aac4ae31529641e82cc3c21e790873b7c826dca
Oleg1989 <ivanishin9891@gmail.com>
2021-04-20 20:53:37 +0300

    reate ToDo

--- Classwork/20.04.21/src/index.ts
+++ Classwork/20.04.21/src/index.ts
@@ -1,6 +1,30 @@
-let a: boolean = false;
-a = true;
-let n: number;
-let s: string;
-let arrN: number[];
+type ToDoId = number;
+
+interface ElementToDo {
+    id: ToDoId;
+    text: string;
+    body: string;
+    deleted: boolean;
+}
+
+class ToDo implements ElementToDo {
+    id: number;
+    text: string;
+    body: string;
+    deleted: boolean;
+
+    constructor(params: ElementToDo){
+        this.id = params.id;
+        this.text = params.text;
+        this.body = params.body;
+        this.deleted = params.deleted;
+    }
+}
+
+const toDo: ToDo = new ToDo({
+    id: 1,
+    text: "Hello",
+    body: "Body",
+    deleted: false
+});
 
