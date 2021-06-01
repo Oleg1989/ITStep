246fd8d89557fafe923cd09eccc5cd9580a50aef
Oleg1989 <ivanishin9891@gmail.com>
2021-06-01 22:43:02 +0300

    Classwork 01.06.21

--- Classwork/25.05.21/src/index.ts
+++ Classwork/25.05.21/src/index.ts
@@ -1,12 +1,19 @@
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import '../node_modules/materialize-css/dist/js/materialize.min';
 import { io, Socket } from "socket.io-client";
+import axios from 'axios';
+
+interface RooomsInterface {
+    id: string;
+    title: string;
+}
 
 const socket: Socket = io("ws://bt-21-playground-vppfc.ondigitalocean.app/");
 
 const nicks = document.getElementById('nicks');
 const screen = document.getElementById('screen');
 const connected = document.getElementById('connected');
+
 
 document.addEventListener("DOMContentLoaded", function (event: Event) {
     document.getElementById('add-nick')?.addEventListener('click', addNickName);
@@ -17,12 +24,8 @@ document.addEventListener("DOMContentLoaded", function (event: Event) {
             let counterAnonym: number = 0;
             let counterUsers: number = 0;
             let counterRegisteredUsers: number = 0;
-            console.log(users);
             for (let key in users) {
                 if (users[key] !== 'Anonymous') {
-                    if (nicks) {
-                        nicks.parentNode?.removeChild(nicks);
-                    }
                     let li = document.createElement('li');
                     li.classList.add('collection-item');
                     li.textContent = `${users[key]}`;
@@ -34,21 +37,22 @@ document.addEventListener("DOMContentLoaded", function (event: Event) {
                 counterUsers++;
             }
             if (counterRegisteredUsers === 0) {
-                if (nicks) {
-                    nicks.parentNode?.removeChild(nicks);
-                }
                 let li = document.createElement('li');
                 li.classList.add('collection-item');
                 li.textContent = 'There are no registered users!';
                 document.getElementById('nicks')?.append(li);
             }
-            let anonym = document.createElement('p');
+            let anonym = document.createElement('li');
             anonym.textContent = `Anonym - ${counterAnonym}`
+            anonym.classList.add('collection-item');
             document.getElementById('anonym')?.append(anonym);
 
-            let total = document.createElement('p');
+            let total = document.createElement('li');
             total.textContent = `Total online - ${counterUsers}`;
-            document.getElementById('total')?.append(total);
+            total.classList.add('collection-item');
+            document.getElementById('anonym')?.append(total);
+
+            getRooms();
         });
 
         socket.on("new_user_connected", (socketId) => {
@@ -60,10 +64,21 @@ document.addEventListener("DOMContentLoaded", function (event: Event) {
         socket.on("new_message", (message: string, socketId) => {
             let li = document.createElement('li');
             li.classList.add('collection-item');
+            li.style.borderRadius = '5px 5px 5px 0';
             li.textContent = `${message}`;
             screen?.append(li);
             console.log(`Message from ${socketId}: ${message}`);
         });
+        socket.on('user_disconnected', userId => console.log(`User disconnected: ${userId}`))
+        const ROOM_ID = 1;
+        socket.emit('join_room', ROOM_ID)
+        socket.on('join_success', (roomId) => { })
+        socket.on('user_joined_room', (userId, roomId) => { })
+        socket.emit('leave_room', ROOM_ID)
+        socket.on('leave_success', (roomId) => { })
+        socket.on('user_left_room', (userId, roomId) => { })
+        socket.emit('msg_to_room', "Message", ROOM_ID)
+        socket.on('new_message_to_room', (message, roomId, userId) => { })
     });
 });
 
@@ -91,4 +106,21 @@ const sendMassage = () => {
             connected.classList.add('red-text');
         }
     });
+}
+async function getRooms() {
+    let response = await window.fetch('https://bt-21-playground-vppfc.ondigitalocean.app/rooms');
+
+    if (response.ok) {
+        let rooms: RooomsInterface[] = await response.json();
+        rooms.forEach(element => {
+            let room = document.createElement('li');
+            room.classList.add('collection-item');
+            room.textContent = element.title;
+            room.id = `${element.id}`;
+            document.getElementById('rooms')?.append(room);
+        });
+
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
 }
