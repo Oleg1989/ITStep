971623699f3bc1f7e5f1f460d68108591d68cf0e
Oleg1989 <ivanishin9891@gmail.com>
2021-06-08 21:12:46 +0300

    Classwork 08.06.21

--- Homework/Week_32_chat/src/demo.ts
+++ Homework/Week_32_chat/src/demo.ts
@@ -1,75 +1,34 @@
-interface IObserver {
-    notify(): void;
+let str: string = 'AADAAFFF';
+
+const packingStr = (str: string) => {
+    let counter: number = 1;
+    let newStr: string = ''
+    let letter: string = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == str[i + 1]) {
+            counter++;
+            letter = str[i];
+        } else {
+            letter = str[i];
+            if (counter == 1) {
+                newStr += `${letter}`;
+            } else {
+                newStr += `${letter}${counter}`;
+            }
+            letter = "";
+            counter = 1;
+        }
+    }
+    return newStr;
 }
+const unpackingStr = (str: string) => {
 
-interface IEvent {
-    trigger(): void;
-    subscribe(observer: IObserver): void;
 }
+const save = () => {
+    let arr
 
-// class User {
-//     private _firstName: string;
-//     private _lastName: string;
-//     private _nickname: string;
-
-//     constructor(firstName: string, lastName: string, nickname: string) {
-//         this._firstName = firstName;
-//         this._lastName = lastName;
-//         this._nickname = nickname;
-//     }
-//     get name(): string {
-//         return `${this._firstName} "${this._nickname}" ${this._nickname}`;
-//     }
-// }
-
-// class UsersUpdatedEvent implements IEvent {
-//     private _subscribers = [];
-//     trigger() {
-//         this._subscribers.forEach(s => s.notify());
-//     }
-//     subscribe(observer: IObserver) {
-//         this._subscribers.push(observer);
-//     }
-// }
-// class UserList {
-//     private _userList = [];
-//     private _usersUpdatedEvent: UsersUpdatedEvent;
-//     constructor(userUpdatedEvent: UsersUpdatedEvent) {
-//         this._usersUpdatedEvent = userUpdatedEvent;
-//     }
-//     add(newUser: User) {
-//         this._userList.push(newUser);
-//         this._usersUpdatedEvent.trigger();
-//     }
-//     get users(): User[] {
-//         return [...this._userList];
-//     }
-// }
-
-// class UsersView implements IObserver {
-//     private _usersList: UserList;
-//     constructor(userList: UserList, userUpdatedEvent: UsersUpdatedEvent) {
-//         this._usersList = userList;
-//         userUpdatedEvent.subscribe(this);
-//     }
-//     render() {
-//         this._usersList.users.forEach(user => console.log(user.name));
-//     }
-//     notify() {
-//         this.render();
-//     }
-// }
-
-// let user1 = new User('User1', 'LastName1', 'nickname1');
-// let user2 = new User('User2', 'LastName2', 'nickname2');
-
-// const userUpdatedEvent = new UsersUpdatedEvent();
-
-// const userList = new UserList(userUpdatedEvent);
-
-// userList.add(user1);
-// userList.add(user2);
-
-// const userView = new UsersView(userList, userUpdatedEvent);
-
-// userList.add(new User('User3', 'LastName3', 'nickname3'));
+}
+const multiplication = (a: number, b: number) => {
+    return a * b;
+}
+console.log(packingStr(str));
