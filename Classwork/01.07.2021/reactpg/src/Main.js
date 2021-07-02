f942244d8210457047c3052e0763ba929705c1df
Oleg1989 <ivanishin9891@gmail.com>
2021-07-02 20:45:16 +0300

    Classwork 01.07.2021 done

--- Classwork/01.07.2021/reactpg/src/Main.js
+++ Classwork/01.07.2021/reactpg/src/Main.js
@@ -1,4 +1,8 @@
+import { Item } from "./Item";
 
 export function Main() {
-    return <div className="main">Hello</div>;
+    return <div className="main">
+        <h1>To do list</h1>
+        <Item />
+    </div>;
 }
