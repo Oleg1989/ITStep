7750da20d3ad7162e04ac07ca7c34aa6af0b5634
Oleg1989 <ivanishin9891@gmail.com>
2021-09-27 21:29:55 +0300

    Week_38 done

--- Homework/Week_38/reactpgts/src/TextArea.tsx
+++ Homework/Week_38/reactpgts/src/TextArea.tsx
@@ -9,8 +9,7 @@ interface State {
 };
 
 export default function Editor() {
-    const [state, setState] = useState<State>({ currentState: { article: "" }, stateData: [] });
-    console.log(state);
+    const [state, setState] = useState<State>({ currentState: { article: '' }, stateData: [] });
     const onArticleEdit = (event: ChangeEvent<HTMLTextAreaElement>) => {
         setState((stateData) => ({
             ...state,
@@ -21,26 +20,43 @@ export default function Editor() {
     const onUndo = () => {
         const stateItem = { ...state.currentState };
         const newCurrentState = state.stateData.pop();
-        if (newCurrentState)
-            setState({
-                ...state,
-                currentState: newCurrentState,
-                stateData: [stateItem, ...state.stateData],
-            });
+        if (newCurrentState) {
+            if (!!stateItem.article) {
+                setState({
+                    ...state,
+                    currentState: newCurrentState,
+                    stateData: [stateItem, ...state.stateData]
+                });
+            } else {
+                setState({
+                    ...state,
+                    currentState: stateItem,
+                    stateData: [...state.stateData, newCurrentState]
+                });
+            }
+        }
     };
     const onRedo = () => {
         const newCurrentState = state.stateData.shift();
-        if (newCurrentState)
-            setState({
-                ...state,
-                currentState: newCurrentState,
-                stateData: [...state.stateData, state.currentState],
-            });
+        if (newCurrentState) {
+            if (!!newCurrentState.article) {
+                setState({
+                    ...state,
+                    currentState: newCurrentState,
+                    stateData: [...state.stateData, state.currentState],
+                });
+            } else {
+                setState({
+                    ...state,
+                    stateData: [newCurrentState, ...state.stateData],
+                });
+            }
+        }
     };
     return (
         <>
-            <br />
-            <textarea rows={12} cols={80} value={state.currentState.article} onChange={onArticleEdit}></textarea><br />
+            <h1>Editor</h1>
+            <textarea rows={24} cols={150} value={state.currentState.article} onChange={onArticleEdit}></textarea><br />
             <button onClick={onUndo}>Undo</button>
             <button onClick={onRedo}>Redo</button>
         </>);
